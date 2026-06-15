1103b649c2c1b9e9f267dfb28960567b9c64e164
Mati Bordonaro <matiasbordonarokpo@gmail.com>
2026-06-15 20:34:17 -0300

    feat(notification-service): agrego manejo de excepciones

---
+++ tpe-farmacy-food/notification-service/src/main/java/com/farmacyfood/notification/exception/SuscripcionNotFoundException.java
@@ -0,0 +1,8 @@
+package com.farmacyfood.notification.exception;
+
+public class SuscripcionNotFoundException extends RuntimeException {
+
+    public SuscripcionNotFoundException(String message) {
+        super(message);
+    }
+}
